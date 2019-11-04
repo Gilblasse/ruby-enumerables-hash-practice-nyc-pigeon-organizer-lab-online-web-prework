35cc03b5e45e17ffda4da027af67bb130be507a2
Nethelbert <gilblasse@gmail.com>
2019-11-04 03:03:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  binding.pry
+  	bird_names = data[:gender].map {|key,value| value}.flatten
+	
+	pigeon_data =	bird_names.each_with_object({}) do |name, h|  
+			h[name] = {
+				:color => [],
+				:gender => [],
+				:lives => [],
+			}
+
+			[:color,:gender,:lives].each do |key|
+				data[key].map do |k,v| 
+					[k => v.each{|val| h[name][key] << k if val == name}]
+				end
+			end
+			
+		end
+
+	pigeon_data
 end
